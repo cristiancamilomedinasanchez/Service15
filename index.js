5dcce3d3587beb8a2b7e39c3676dcfc7b0e2497c
cookeronconnection77 <cookeronconnection77@gmail.com>
2025-03-20 18:24:49 -0500

    actualizando url

--- index.js
+++ index.js
@@ -18,7 +18,7 @@ const storage = multer.diskStorage({
 let mesas = {};  
 const fs = require("node:fs")
 
-const frontendUrl = "https://cookeronconnection77.github.io";
+const frontendUrl = "https://cookeronconnection.com";
 
 
 // app.use(multer({storage: storage, dest: path.join(__dirname, "./public/images")}).single("image"))
